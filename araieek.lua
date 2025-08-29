1ba7088d678e5f6557fe8d78871e27852989addf
T0nkla <69423782+Tonklakubb@users.noreply.github.com>
2025-08-29 21:12:31 +0700

    Update araieek.lua

--- araieek.lua
+++ araieek.lua
@@ -2,17 +2,32 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
 local player = Players.LocalPlayer
-local WEBAPP_URL = "https://script.google.com/macros/s/AKfycby1vo1rw_JhDGBXF5pUwHMkKXxL8hpyNyRWjUz_bUd4_X-SBZzrKVhjxp2VKzj40WkR/exec"
+
+local WEBAPP_URL = "https://script.google.com/macros/s/AKfycbygaCcVwtue3fZpgZaOvp9DPGAg59_TrktBlm4NQFfkvEdHPksMY3KYkDoFJq0pZCnX/exec"
 local SHARED_TOKEN = "Testing"
 
+local function sendToSheet()
+    local diamonds = player:GetAttribute("Diamonds") or 0
+    local note = "หากลูกค้าฟาร์มเสร็จ ชื่อตัวละคร Roblox ของคุณจะหายไปและจะถูกแจ้งเตือนไปยังช่องทางที่ท่านจ้างงานเรา"
+
+
+    if diamonds >= (_G.diamonds or 0) then
+        note = "เสร็จ"
+    end
+
+    local qs = "token="..HttpService:UrlEncode(SHARED_TOKEN)
+        .."&playerName="..HttpService:UrlEncode(player.Name)
+        .."&diamonds="..HttpService:UrlEncode(tostring(diamonds))
+        .."&note="..HttpService:UrlEncode(note)
+
+    local url = WEBAPP_URL .. (WEBAPP_URL:find("?",1,true) and "&" or "?") .. qs
+    local res = HttpService:RequestAsync({ Url = url, Method = "GET" })
+    print("SEND GET ->", res.StatusCode, res.Success, res.Body)
+end
+
+
 task.wait(2)
-local diamonds = player:GetAttribute("Diamonds") or 0
+sendToSheet()
 
-local qs = "token="..HttpService:UrlEncode(SHARED_TOKEN)
-    .."&playerName="..HttpService:UrlEncode(player.Name)
-    .."&diamonds="..HttpService:UrlEncode(tostring(diamonds))
-    .."&note="..HttpService:UrlEncode("หากลูกค้าฟาร์มเสร็จ ชื่อตัวละคร Roblox ของคุณจะหายไปและจะถูกแจ้งเตือนไปยังช่องทางที่ท่านจ้างงงานเรา")
 
-local url = WEBAPP_URL .. "?" .. qs
-local res = HttpService:RequestAsync({ Url = url, Method = "GET" })
-print(res.StatusCode, res.Success, res.Body)
+player:GetAttributeChangedSignal("Diamonds"):Connect(sendToSheet)
