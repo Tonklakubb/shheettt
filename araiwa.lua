9f488294703587cad4f322c38dbe2e7d3403eace
T0nkla <69423782+Tonklakubb@users.noreply.github.com>
2025-09-06 19:26:35 +0700

    Update araiwa.lua

--- araiwa.lua
+++ araiwa.lua
@@ -2,17 +2,41 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
 local player = Players.LocalPlayer
-local WEBAPP_URL = "https://script.google.com/macros/s/AKfycby1vo1rw_JhDGBXF5pUwHMkKXxL8hpyNyRWjUz_bUd4_X-SBZzrKVhjxp2VKzj40WkR/exec"
+
+local WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzdzwOsmd-n89SJVqolHyiwkrBxwlCyNN-OyGEAvzuM4bc-uyJ8iEzzx5JAghX4OdKs/exec"
 local SHARED_TOKEN = "Testing"
 
+local function sendToSheet()
+    local diamonds = player:GetAttribute("Diamonds") or 0
+    local note = "กำลังฟาม"
+
+
+    if diamonds >= (_G.diamonds) then
+        note = "เสร็จ"
+    end
+
+    local qs = "token="..HttpService:UrlEncode(SHARED_TOKEN)
+        .."&playerName="..HttpService:UrlEncode(player.Name)
+        .."&diamonds="..HttpService:UrlEncode(tostring(diamonds))
+        .."&note="..HttpService:UrlEncode(note)
+
+    local url = WEBAPP_URL .. (WEBAPP_URL:find("?",1,true) and "&" or "?") .. qs
+    local res = HttpService:RequestAsync({ Url = url, Method = "GET" })
+    print("SEND GET ->", res.StatusCode, res.Success, res.Body)
+end
+
+
 task.wait(2)
+sendToSheet()
+
+
+player:GetAttributeChangedSignal("Diamonds"):Connect(sendToSheet)
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
 local diamonds = player:GetAttribute("Diamonds") or 0
-
-local qs = "token="..HttpService:UrlEncode(SHARED_TOKEN)
-    .."&playerName="..HttpService:UrlEncode(player.Name)
-    .."&diamonds="..HttpService:UrlEncode(tostring(diamonds))
-    .."&note="..HttpService:UrlEncode("Current Diamonds")
-
-local url = WEBAPP_URL .. "?" .. qs
-local res = HttpService:RequestAsync({ Url = url, Method = "GET" })
-print(res.StatusCode, res.Success, res.Body)
+if diamonds >= (_G.diamonds) then
+    wait(10)
+    game.Players.LocalPlayer:Kick("Done")
+end
